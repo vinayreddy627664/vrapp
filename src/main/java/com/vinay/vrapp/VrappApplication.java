faf517978bfd4fa58ab4a63ea4766a838574a393
VINAYBEEREDDY <VINAYBEEREDDY@192.168.0.13>
2016-08-02 22:03:57 -0500

    project init

---
+++ src/main/java/com/vinay/vrapp/VrappApplication.java
@@ -0,0 +1,12 @@
+package com.vinay.vrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vinay/vrapp/VrappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vinay.vrapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VrappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
